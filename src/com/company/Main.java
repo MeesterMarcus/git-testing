e328c15d0afd39eb14af2f319dba94e4662c5dc1
MeesterMarcus <lorenzanamarcus@gmail.com>
2020-08-23 10:19:48 -0500

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// change the message
         System.out.println("testing git functions");
     }
 }
