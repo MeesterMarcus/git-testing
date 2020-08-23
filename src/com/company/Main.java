2409e1569ac80d94b9e721dc5431c882787a569d
MeesterMarcus <lorenzanamarcus@gmail.com>
2020-08-23 10:44:43 -0500

    adding for loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// change the message 4
+        // change the message 4
         System.out.println("testing git functions");
+        System.out.println("see below");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("index:" + i);
+        }
     }
 }
