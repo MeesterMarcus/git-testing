5f485f00f8c0ee1c4eb2f6587880229ca1bddace
marcus.lorenzana <marcus.lorenzana@ilendx.com>
2020-08-24 10:12:18 -0500

    changing the message to 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// change the message 4
+	// change the message 5
         System.out.println("testing git functions");
     }
 }
